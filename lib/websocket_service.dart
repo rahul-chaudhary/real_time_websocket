783ab76a9d9a83ebbbe9fa0480e252edea5382a6
Rahul Chaudhary <bossrc64@outlook.com>
2025-04-03 14:23:33 +0530

    update WebSocket connection URL to use secure WebSocket protocol

--- lib/websocket_service.dart
+++ lib/websocket_service.dart
@@ -5,7 +5,7 @@ import 'package:web_socket_channel/web_socket_channel.dart';
 
 class RealtimeDataService {
   void start(StreamController<Map> controller){
-    var channel = WebSocketChannel.connect(Uri.parse("http://192.168.1.33:3000/"));
+    final channel = WebSocketChannel.connect(Uri.parse("ws://192.168.1.33:3000/ws"));
     channel.stream.listen((event) {
       Map data = bson.BsonCodec.deserialize(bson.BsonBinary.from(event));
       controller.add(data);
