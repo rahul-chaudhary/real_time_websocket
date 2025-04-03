425b91f180b65d00b9bbdc4270fb45a7e63edd49
Rahul Chaudhary <bossrc64@outlook.com>
2025-04-03 15:13:07 +0530

    refactor DataProvider to simplify stream handling and remove unnecessary StreamController

--- lib/provider/data_provider.dart
+++ lib/provider/data_provider.dart
@@ -5,33 +5,21 @@ import '../websocket_service.dart';
 
 class DataProvider extends ChangeNotifier {
   final RealtimeDataService service;
-  late StreamController<Map> _streamController;
   RealtimeDataModel? _data;
 
   DataProvider({required this.service}) {
-    _streamController = StreamController<Map>.broadcast();
-  }
-
-  RealtimeDataModel? get data => _data;
-
-  void startListening() {
-    service.start(_streamController);
-    _streamController.stream.listen((data) {
-      print("Received Data: $data");  // Debugging
+    service.start();
+    service.stream.listen((data) {
       _updateData(data);
     });
   }
 
+  RealtimeDataModel? get data => _data;
+
   void _updateData(Map data) {
-    print("Updating Data: ${data.toString()}");  // Debugging
     _data = RealtimeDataModel.fromMap(data);
     notifyListeners();
   }
-
-  @override
-  void dispose() {
-    _streamController.close();
-    super.dispose();
-  }
 }
+
 
